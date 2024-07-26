90d3adf2a0603075e5282e6b0dc6290d09616120
Eduardo Brennand <eduardolirabrennand@gmail.com>
2024-07-25 22:37:15 -0300

    Retira as colunas de datetime do retorno

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from pydantic import BaseModel, ConfigDict, EmailStr
 
 from fast_zero.models import TodoState
@@ -43,8 +41,6 @@ class TodoSchema(BaseModel):
 
 class TodoPublic(TodoSchema):
     id: int
-    created_at: datetime
-    updated_at: datetime | None = None
 
 
 class TodoList(BaseModel):
